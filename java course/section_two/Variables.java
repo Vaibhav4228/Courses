21ada6322408492d6de3f4d809c41cfa720c6670
Vaibhav4228 <vaibhavsharma88890@gmail.com>
2025-01-09 12:15:30 +0530

    complted challnge 1 work book and varibales

---
+++ java course/section_two/Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+    }
+  
+    
+}
+
---
+++ java course/section_two/bus.java
@@ -0,0 +1,8 @@
+public class bus {
+public static void main(String[] args) {
+    int mySecondNumber = 0;
+    mySecondNumber= mySecondNumber+9;
+
+    System.out.println(mySecondNumber);
+}
+}
