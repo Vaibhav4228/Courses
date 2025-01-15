56b5458bf2b02bfd89b5ce5442e8caf70f42bb3c
Vaibhav4228 <vaibhavsharma88890@gmail.com>
2025-01-15 13:01:38 +0530

    Use stringutils and added dependency to lang3

--- maven-course/src/main/java/clinic/programming/training/Application.java
+++ maven-course/src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,33 @@
 package clinic.programming.training;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
+
+    public int countWords(String words){
+        String[] separateWords = StringUtils.split(words, " ");
+        return (separateWords == null) ? 0 : separateWords.length;
+    }
+
+    public void hello(){
+        List<String> list = new ArrayList<String>();
+        list.add("Hello");
+
+        for (String s : list) {
+            System.out.println("hello bro: " + s);
+        }
+    }
     
     public Application() {
         System.out.println ("Inside Application");
     }
-
-    // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+    app.hello();
+    int count = app.countWords("how are you? I am vaibhav");
+    System.out.println("Word count: " + count);
     }
 }
