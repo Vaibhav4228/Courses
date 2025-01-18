531cc57879514825699985bb5530b85a0ca1431c
Vaibhav4228 <vaibhavsharma88890@gmail.com>
2025-01-18 20:47:24 +0530

    implemented swagger and openAPI spec

--- spring course/rest-api/src/main/java/com/vaibhav_effigo/rest_api/helloWorldApi/HelloWorldController.java
+++ spring course/rest-api/src/main/java/com/vaibhav_effigo/rest_api/helloWorldApi/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.vaibhav_effigo.rest_api.helloWorldApi;
 
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
 
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
     @GetMapping("/hello-world")
     public String helloWorld(){
         return "Hello, I am HELLO World Api..";
@@ -15,6 +24,15 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean ("Hello, I am HELLO World Bean Api..");
+
+    }
+
+    @GetMapping("/hello-world-bean-internationalized")
+    public String helloWorldinternationalized(){
+
+        Locale locale = LocaleContextHolder.getLocale();
+      return  messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
 
     }
 
